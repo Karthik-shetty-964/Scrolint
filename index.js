5310a5ea227bf8099d4f6556f06c68d55c02d050
Karthik shetty <karthiksshetty111@gmail.com>
2022-12-21 21:04:37 +0530

    I'm doe

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json({limit : "30mb" , extended : true}));
 app.use(bodyParser.urlencoded({limit : "30mb" , extended : true}));
 app.use(cors());
+app.use("/assets",express.static(path.join(__dirname,'public/assets')));
 
 
 
@@ -42,7 +43,6 @@ app.get('*', function(req,res){
     res.sendFile(path.join(__dirname,'./client/build/index.html'));
 });
 
-app.use("/assets",express.static(path.join(__dirname,'public/assets')));
 
 //FILE STORAGE
 /*This is to store any files that is uploaded to our website in 'pulic/assets' folder. */
